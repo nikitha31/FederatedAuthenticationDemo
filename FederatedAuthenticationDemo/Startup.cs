60d724122315fe2901f29e751e789937f0faedf8
Nikitha Chittaluri <Nikitha_Chittaluri@epam.com>
2020-03-20 09:29:33 +0530

    Add project files.

---
+++ FederatedAuthenticationDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FederatedAuthenticationDemo.Startup))]
+namespace FederatedAuthenticationDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
